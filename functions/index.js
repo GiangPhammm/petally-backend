958c23bd79ecaf158f6eb4d6fdd6219d62fa5fc0
Giang Pham <phamthugiang2604@gmail.com>
2024-02-28 22:07:44 +0100

    signup api

--- functions/index.js
+++ functions/index.js
@@ -1,14 +1,40 @@
 const functions = require('firebase-functions');
-const admin = require('firebase-admin');
 const express = require('express');
-const cors = require('cors');
+
+// Grant firebase admin right
+const admin = require('firebase-admin');
+var serviceAccount = require('./secrets/permissions.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
 
 const app = express();
+const db = admin.firestore();
 
-// Route
-app.get('/hello', (req, res) => {
-    return res.status(200).send('Hi there')
+// Allow another domain origin
+const cors = require('cors');
+app.use(cors({origin: true}));
+
+// Signup
+app.post('/signup', (req, res) => {
+    (async () => {
+        try {
+            await db.collection('accounts').doc('/' + req.body.id + '/')
+                .create({
+                    username: req.body.username,
+                    email: req.body.email,
+                    password: req.body.password,
+                })
+            return res.status(200).send();
+        } catch (error) {
+            console.log(error);
+            return res.status(500).send(error);
+        }
+    })();
 })
+
+
 
 // Export api to firebase cloud functions
 exports.app = functions.https.onRequest(app)
